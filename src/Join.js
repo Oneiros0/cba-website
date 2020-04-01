875d550c42e090ff1d6fa5f2c68fe93999ab0426
element.cr@gmail.com <element.cr@gmail.com>
2020-04-01 17:15:03 -0400

    Changes to Join Screen & Membership Flow

--- src/Join.js
+++ src/Join.js
@@ -12,16 +12,24 @@ function Join() {
             CBA - Annual Membership
           </h2>
           <p>
-            For only $30 a year, you can be apart of the Chattanooga Beverage Alliance!  This membership is an annual fee.
+            For only $30 a year, you can be a member of the Chattanooga Beverage Alliance! This membership is an annual fee.
           </p>
         </div>
+
+
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_s-xclick"/>
+        <input type="hidden" name="cmd" value="_s-xclick"/>
+        <input type="hidden" name="hosted_button_id" value="KQAEW4K7MQHJA"/>
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+        
+
+          {/* <input type="hidden" name="cmd" value="_s-xclick"/>
           <input type="hidden" name="hosted_button_id" value="BGL84A6NJQ3YN"/>
           <table>
           </table>
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/> */}
         </form>
         <Image id="cheers" src={cheers}/>
         <br/>
